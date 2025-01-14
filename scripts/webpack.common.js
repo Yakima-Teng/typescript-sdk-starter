3bcd03fa724573b0c0fae72308fe31b7de54221e
tengyunfeng <tengyunfeng@gongguizhijia.com>
2025-01-14 11:47:05 +0800

    refactor: rename built file name

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     clean: true,
     // 打包后的产物名
-    filename: `${pkgName}.min.js`,
+    filename: `${pkgName}.js`,
     // 在全局变量中增加一个全局变量用于访问SDK，如 window.TypescriptSdkStarter
     library: toCamelCase(pkgName),
     // 打包成umd模块
